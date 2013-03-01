aca8804a268862ea95c5a681a85fe5562edb2f09
Gustavo Garcia <luctus@gmail.com>
2013-03-01 00:48:00 -0300

    omnioauth tokens untracked

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "Wwdw1V1WIUgKV3cJOtiLA", "0WkyLJvtgoXNqIEAd9Mh68etMw42y4jaZgShdXLLo"
-end
